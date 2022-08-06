9d5094fcbb1426853a327a4248d460959427d6e7
AFK <96073081+afk-0@users.noreply.github.com>
2022-08-07 02:50:44 +0600

    Create 1. Solve Me First.kt

---
+++ Problem Solving/Hacker Rank/1. Solve Me First.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main (args : Array<String>){
+
+    var scanner = Scanner(System.`in`)
+
+    var a = scanner.nextInt()
+    var b  = scanner.nextInt()
+
+    println(a+b)
+}
