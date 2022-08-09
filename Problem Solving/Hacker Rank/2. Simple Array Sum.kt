32b411fd03b65002edd5a4fd45bcdd59de3b3b92
AFK <96073081+afk-0@users.noreply.github.com>
2022-08-09 11:05:20 +0600

    Create 2. Simple Array Sum.kt

---
+++ Problem Solving/Hacker Rank/2. Simple Array Sum.kt
@@ -0,0 +1,18 @@
+import java.util.Scanner
+
+fun main (args : Array<String>){
+    var scanner = Scanner(System.`in`)
+
+    var size = scanner.nextLine().trim().toInt()
+    var ar = scanner.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    var sum = 0
+
+    for (x in ar){
+        sum = sum + x
+    }
+
+    println(sum)
+
+
+}
